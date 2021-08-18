8e72a76959c156b026e85a9fdce0db02e6187077
glrepot <tfgab34@gmail.com>
2021-08-18 12:13:45 -0300

    Prueba google

--- js/login.js
+++ js/login.js
@@ -8,12 +8,9 @@ document.addEventListener("DOMContentLoaded", function(e){
 function rojo(input_id, input_error){
     var id_ingresada = document.getElementById(input_id);
     var error_ingresado = document.getElementById(input_error);
-    if(id_ingresada.value == ''){
-        error_ingresado.innerHTML = "Complete el campo por favor";
-        console.log("no hay datos ingresados");
-    } else if(id_ingresada.value.length < 5) {
+    if(id_ingresada.value.length < 5) {
         error_ingresado.innerHTML = "Ingrese mínimo 5 carácteres";
-        console.log("datos muy cortos");
+        console.log("no hay datos o son muy pocos");
     } else {
         error_ingresado.innerHTML = "";
         console.log("hay datos ingresados");
