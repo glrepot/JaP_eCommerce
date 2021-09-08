fe74a23f30174a17897874b548883d9013fb426d
glrepot <tfgab34@gmail.com>
2021-09-08 09:07:48 -0300

    product info basico funcionando

--- js/product-info.js
+++ js/product-info.js
@@ -1,29 +1,35 @@
 var comments_array = []; //array que contendrá los comentarios
 var products_array = []; //array que contendrá los productos
 
-function showProductsPro(array_to){
-    var htmlContentToAppend = ""; 
-    for(let i = 0; i < array_to.length; i++){
-        let pro = array_to[i];
 
-        htmlContentToAppend += `<p>` + pro.user + `<p>`;
-
-        document.getElementById("prod_div").innerHTML = htmlContentToAppend; 
-    };
+function showProductPro(array_to){
+	var div_del_html = document.getElementById("prod_div");
+	
+	div_del_html.innerHTML = array_to.name + `<br>` + array_to.description + `<br>` + array_to.cost +
+		array_to.soldCount + `<br>` + array_to.category + `<br>`;
+	
+	var imgsToAppend = "";
+	for (let i = 0; i < array_to.length; i++){
+		let pro = array_to[i];
+		
+		imgsToAppend = `<br>` + pro.images;
+		
+		div_del_html.innerHTML = imgsToAppend;
+	};
 };
 
-/*
+
 function showProductsCom(array_to){
     var htmlContentToAppend = ""; 
     for(let i = 0; i < array_to.length; i++){
         let com = array_to[i];
 
-        htmlContentToAppend += `<li>`
+        htmlContentToAppend += `<p>` + com.user + `<p>`;
 
-        document.getElementById("yes").innerHTML = htmlContentToAppend; 
+        document.getElementById("com_div").innerHTML = htmlContentToAppend; 
     };
 };
-*/
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -34,16 +40,16 @@ document.addEventListener("DOMContentLoaded", function(e){
         console.log("Check status: " + check.status);
         if (check.status === 'ok'){
             products_array = check.data;
-            showProductsPro(products_array);
+            showProductPro(products_array);
         };
     });
 	
-	/*
+	
 	getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(check){
         console.log("Check status: " + check.status);
         if (check.status === 'ok'){
             comments_array = check.data;
             showProductsCom(comments_array);
         };
-    });*/
+    });
 });
