e91e107d95e7a414d0b176614205e2d93b007a94
glrepot <tfgab34@gmail.com>
2021-10-25 08:23:41 -0300

    recordatorio

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,9 @@
+/*
+TIENE QUE USAR OBLIGATORIAMENTE UN JSON.STRINGIFY
+COMO DICE EN LA LETRA:
+Recuerda del uso de los métodos JSON.stringify y JSON.parse para poder almacenar y recuperar los datos almacenados respectivamente.
+*/
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
